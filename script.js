ffe104f41f3ce3644f587fd9f4097e77caaec5aa
CaptCupcake21 <58839387+CaptCupcake21@users.noreply.github.com>
2024-04-04 19:00:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-function solveQuadratic() {
-    const a = document.getElementById('a-coef').value;
-    const b = document.getElementById('b-coef').value;
-    const c = document.getElementById('c-coef').value;
-    
-    const rootPart = Math.sqrt(b * b - 4 * a * c);
-    const denom = 2 * a;
-    
-    const root1 = (-b + rootPart) / denom;
-    const root2 = (-b - rootPart) / denom;
-    
-    const solution = document.getElementById('solution');
-    
-    if(rootPart > 0) {
-        solution.innerHTML = `Roots are real and different: <br> x1 = ${root1.toFixed(2)} <br> x2 = ${root2.toFixed(2)}`;
-    } else if(rootPart === 0) {
-        solution.innerHTML = `Roots are real and same: <br> x = ${root1.toFixed(2)}`;
-    } else {
-        solution.innerHTML = "No Real Solution";
-    }
+function openWeaponCalculator() {
+    alert("Weapon Calculator Coming Soon!");
+}
+
+function openDriveCalculator() {
+    alert("Drive Calculator Coming Soon!");
+}
+
+function openPowerCalculator() {
+    alert("Power Calculator Coming Soon!");
+}
+
+function openOverallCalculator() {
+    alert("Overall Characteristics Calculator Coming Soon!");
 }
